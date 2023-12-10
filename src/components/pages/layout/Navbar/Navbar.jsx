5f4e039f60eccf2193012bfcdce406d111335870
dylann97 <d.xavero@hotmail.com>
2023-12-11 00:07:28 +0100

    💄  F01: Add style to the titles

--- src/components/pages/layout/Navbar/Navbar.jsx
+++ src/components/pages/layout/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import NavbarLinks from "./NavbarLinks";
 import Logo from "../../../reusable-ui/Logo";
-import NavbarSocials from "./NavbarSocials";
+import SocialIcon from "./SocialIcon";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center h-[15dvh] absolute w-full border-b-[1px] border-slate-200 ">
       <Logo />
       <NavbarLinks />
-      <NavbarSocials />
+      <div className="flex items-center gap-3">
+        <SocialIcon Icon={<FaGithub />} size="text-2xl" />
+        <SocialIcon Icon={<FaLinkedinIn />} size="text-2xl" />
+      </div>
     </div>
   );
 }
--- src/components/pages/layout/Navbar/NavbarLinks.jsx
+++ src/components/pages/layout/Navbar/NavbarLinks.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import Title from "../../../reusable-ui/Title";
 
 export default function NavbarLinks() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/work">Work</Link>
+    <div className="flex gap-3 text-2xl">
+      <Link to="/" className="hover:underline underline-offset-8">
+        <Title label="Home" />
+      </Link>
+      <Link to="/work" className="hover:underline underline-offset-8">
+        <Title label="Work" />
+      </Link>
+      <Link to="/work" className="hover:underline underline-offset-8">
+        <Title label="About Me" />
+      </Link>
     </div>
   );
 }
