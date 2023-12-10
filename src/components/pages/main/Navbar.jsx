30bc23e39be4d9ce767af87735d9e32184d513f7
dylann97 <d.xavero@hotmail.com>
2023-12-10 10:44:15 +0100

    🚧 F01: Navbar > Set the structure of Navbar component

--- src/components/pages/main/Navbar.jsx
+++ src/components/pages/main/Navbar.jsx
@@ -1,11 +1,13 @@
 import NavbarLinks from "./NavbarLinks";
 import Logo from "../../reusable-ui/Logo";
+import NavbarSocials from "./NavbarSocials";
 
 export default function Navbar() {
   return (
     <div>
       <Logo />
       <NavbarLinks />
+      <NavbarSocials />
     </div>
   );
 }
---
+++ src/components/pages/main/NavbarSocials.jsx
@@ -0,0 +1,10 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+export default function NavbarSocials() {
+  return (
+    <div>
+      <FaGithub />
+      <FaLinkedinIn />
+    </div>
+  );
+}
