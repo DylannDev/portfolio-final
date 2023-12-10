b231a2c2b7d915cabaa05e460de3c1f6e80fb6f1
dylann97 <d.xavero@hotmail.com>
2023-12-10 11:39:52 +0100

    🎨 F01: Layout and app > Improve the general structure of the app

--- src/components/pages/layout/Navbar/Navbar.jsx
+++ src/components/pages/layout/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import NavbarLinks from "./NavbarLinks";
-import Logo from "../../reusable-ui/Logo";
+import Logo from "../../../reusable-ui/Logo";
 import NavbarSocials from "./NavbarSocials";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="flex justify-between">
       <Logo />
       <NavbarLinks />
       <NavbarSocials />
--- src/components/pages/layout/Navbar/NavbarSocials.jsx
+++ src/components/pages/layout/Navbar/NavbarSocials.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 export default function NavbarSocials() {
   return (
-    <div>
+    <div className="flex gap-2 items-center ">
       <FaGithub />
       <FaLinkedinIn />
     </div>
