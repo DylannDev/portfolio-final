70d97f707af33f91dc8baec40d78e724c67011b0
dylann97 <d.xavero@hotmail.com>
2023-12-10 11:37:25 +0100

    🚧 F01: Layout > Add the layout (navbar on every pages)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import "./App.css";
-import Hero from "./components/pages/main/Hero";
-import Navbar from "./components/pages/main/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/pages/home/Home";
+import Work from "./components/pages/work/Work.jsx";
+import ErrorPage from "./components/pages/error/ErrorPage";
+import Layout from "./components/pages/layout/Layout.jsx";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/work",
+          element: <Work />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import WorkPage from "./components/pages/work/WorkPage.jsx";
-import ErrorPage from "./components/pages/error/ErrorPage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/work",
-    element: <WorkPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
