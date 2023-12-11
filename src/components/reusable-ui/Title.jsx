114acf63a5674718d311c1342dab96c98014aa1c
dylann97 <d.xavero@hotmail.com>
2023-12-11 15:48:40 +0100

    🎨 F02: Title > Add description paragraph to the component

--- src/components/reusable-ui/Title.jsx
+++ src/components/reusable-ui/Title.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function Title({ size, label }) {
+export default function Title({ size, label, description }) {
   return (
-    <div className={`font-extrabold ${size ? size : "text-3xl"}`}>
-      <span className="text-primary">/</span>
-      {label}
-      <span className="text-secondary">.</span>
+    <div className={`text-center ${size ? size : "text-4xl mb-16"}`}>
+      <div className="font-extrabold mb-4">
+        <span className="text-primary">/</span>
+        {label}
+        <span className="text-secondary">.</span>
+      </div>
+      {description && <p className="text-lg">{description}</p>}
     </div>
   );
 }
