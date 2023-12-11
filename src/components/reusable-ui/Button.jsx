60d3db8a6b341d179f34a059bcbb951848798b98
dylann97 <d.xavero@hotmail.com>
2023-12-11 11:02:26 +0100

    💄  F01: Button > Set style of the component

--- src/components/reusable-ui/Button.jsx
+++ src/components/reusable-ui/Button.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function Button({ Icon, label }) {
   return (
-    <button className="flex items-center gap-2 rounded-full px-4 py-3 bg-primary hover:bg-primaryLight active:bg-primaryDark text-textLight font-medium">
+    <button className="flex items-center gap-2 rounded-full px-4 py-2 bg-primary hover:bg-primaryLight active:bg-primaryDark text-textLight font-medium">
       <div className="text-xl">{Icon && Icon}</div>
       <span className="">{label}</span>
     </button>
