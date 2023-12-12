889f7b351dc2d98106b696f076b1096333249b92
dylann97 <d.xavero@hotmail.com>
2023-12-12 15:33:39 +0100

    💄  F03: Button > Add final style to the component

--- src/components/reusable-ui/Button.jsx
+++ src/components/reusable-ui/Button.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 export default function Button({ Icon, label }) {
   return (
-    <button
+    <a
       className="flex items-center justify-center gap-2 rounded-full px-4 py-2 bg-primary active:bg-primaryDark 
         hover:shadow-lg text-textLight  shadow-md font-medium text-lg"
+      href="mailto:d.xavero@hotmail.com"
     >
       {Icon && <div className="text-xl">{Icon}</div>}
       <span className="">{label}</span>
-    </button>
+    </a>
   );
 }
