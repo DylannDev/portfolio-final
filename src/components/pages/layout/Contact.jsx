95cc54dd3852af5e0dd37b1bd7f1d0a5f9087e16
dylann97 <d.xavero@hotmail.com>
2023-12-12 15:30:59 +0100

    🚧  F03: Contact > Create component contact

---
+++ src/components/pages/layout/Contact.jsx
@@ -0,0 +1,23 @@
+import Button from "../../reusable-ui/Button";
+import Card from "../../reusable-ui/Card";
+import Section from "../../reusable-ui/Section";
+import Title from "../../reusable-ui/Title";
+import { PiPaperPlaneTilt } from "react-icons/pi";
+
+export default function Contact() {
+  return (
+    <Section>
+      <Title
+        label="Contact"
+        description="Discutons plus en détails de vos idées."
+      />
+      <Card className="flex justify-between w-full p-20">
+        <h2 className="text-3xl font-bold">Un nouveau projet ?</h2>
+        <p className="text-center text-lg">
+          Mon travail vous intéresse? <br /> N’hésitez pas à me contacter 😃
+        </p>
+        <Button label="d.xavero@hotmail.com" Icon={<PiPaperPlaneTilt />} />
+      </Card>
+    </Section>
+  );
+}
