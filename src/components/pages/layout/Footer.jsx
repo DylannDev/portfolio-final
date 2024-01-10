a3955818ea1faf6cdfa584d14f3ffb6622dc439b
dylann97 <d.xavero@hotmail.com>
2024-01-10 13:20:32 +0100

    💄 F05: Footer > Change text color depending on DarkMode

--- src/components/pages/layout/Footer.jsx
+++ src/components/pages/layout/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer() {
   const getYear = date.getFullYear();
 
   return (
-    <div className="flex flex-col gap-8 items-center border-t-[1px] border-slate-200 px-20 pb-20 pt-48 absolute w-full -z-[1]">
+    <div className="flex flex-col gap-8 items-center border-t-[1px] border-slate-200 px-20 pb-20 pt-48 mt-64">
       <div className="flex gap-4">
         {socialIconData.map((icon) => (
           <SocialIcon key={nanoid(8)} Icon={icon.icon} size="text-2xl" />
         ))}
       </div>
-      <div className="text-slate-600">
+      <div>
         <p>Designé et développé par Dylann Xavero.</p>
         <p>Copyright © {getYear}. Tous droits réservés.</p>
       </div>
