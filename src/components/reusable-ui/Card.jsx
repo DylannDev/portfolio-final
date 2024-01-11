31b45858cd9adf12777dd225db392ac35d67a4d3
dylann97 <d.xavero@hotmail.com>
2024-01-11 22:41:24 +0100

    ♻️ F06: Card > Keep component more flexible

--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -2,8 +2,8 @@
 export default function Card({ children, className }) {
   return (
     <div
-      className={`border-[1px] border-slate-200 rounded-3xl  min-w-[280px] shadow-md ${
-        className ? className : "max-w-[360px] p-8"
+      className={`border-[1px] border-slate-200 rounded-3xl min-w-[280px] shadow-md ${
+        className ? className : "p-8"
       }`}
     >
       {children}
