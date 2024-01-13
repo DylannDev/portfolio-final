d05ec1625fbf287b1803b28862abff66df9962f5
dylann97 <d.xavero@hotmail.com>
2024-01-13 12:29:55 +0100

    🚧  F07: About > Build About section

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,3 +1,100 @@
+/* eslint-disable react/no-unescaped-entities */
+import { PiFileArrowDown } from "react-icons/pi";
+import Button from "../../reusable-ui/Button";
+import Section from "../../reusable-ui/Section";
+import Title from "../../reusable-ui/Title";
+import TitleProjects from "../../reusable-ui/TitleProjects";
+import CvDevweb from "/public/cv-dylann-xavero-devweb-2024.pdf";
+import Card from "../../reusable-ui/Card";
+import { softSkillsData } from "../../../data/softSkillsData/softSkillsData";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center pt-[20dvh] ">
+        <Section>
+          <div className="max-w-3xl">
+            <Title
+              label="About Me"
+              description="Découvrez en plus sur moi et mon parcours."
+              className="text-center"
+            />
+            <img
+              src="/images/computer-background.jpg"
+              alt="computer"
+              className="max-w-3xl rounded-3xl mb-12"
+            />
+            <div className="flex justify-between items-start  border-primary  ">
+              <TitleProjects
+                size="text-4xl"
+                label="Background"
+                className="mb-6"
+              />
+            </div>
+            <div className="flex flex-col gap-6 text-justify pb-4 text-lg mb-12">
+              <p>
+                Actuellement en reconversion professionnelle, je me suis pris de
+                passion pour la conception de sites web après mes études. Le
+                fait de pouvoir créer de moi-même mes propres projets m’a tout
+                de suite plu. Ayant également un attrait certain pour le design,
+                c’est tout naturellement que je me suis tourné vers le
+                front-end.
+              </p>
+              <p>
+                Je suis un développeur passionné avec une formation solide en
+                Javascript et une très bonne maîtrise de sa librairie React.
+                J’aime créer des interfaces visuellement agréables et
+                intuitives. Toujours à la recherche de nouveaux défis, chaque
+                nouveau projet est une opportunité de mettre en pratique mes
+                compétences et d’en apprendre toujours plus sur la programmation
+                web.
+              </p>
+              <p>
+                Je suis ouvert aux nouvelles opportunités et collaborations
+                passionnantes. N'hésitez pas à me contacter pour discuter de
+                votre projet ou simplement pour échanger sur le monde du
+                développement web.
+              </p>
+            </div>
+
+            <div className="flex justify-between">
+              <p className="text-xl font-bold pr-4">
+                Besoin d'en savoir plus sur mon parcours ? <br />
+                Téléchargez mon CV en ligne.
+              </p>
+              <Button
+                Icon={<PiFileArrowDown />}
+                label="Voir mon CV"
+                to={CvDevweb}
+              />
+            </div>
+          </div>
+        </Section>
+      </div>
+      <hr />
+      <Section className="mt-20">
+        <Title
+          label="Soft-Skills"
+          description="Zoom sur mes qualités de développeur front-end."
+          className="mb-6"
+        />
+        <div className="flex gap-4 text-center">
+          {softSkillsData.map((skill, index) => (
+            <Card
+              key={index}
+              className="hover:scale-105 transition-all p-8 flex flex-col gap-3"
+            >
+              <div className="min-h-[131px] flex items-center">
+                <img src={skill.img} alt="illustration des qualités" />
+              </div>
+              <h3 className="font-extrabold text-xl capitalize">
+                {skill.quality}
+              </h3>
+              <p>{skill.description}</p>
+            </Card>
+          ))}
+        </div>
+      </Section>
+    </>
+  );
 }
