5b333c5d9d08acc8295e95e4e82b7f49a69d5390
dylann97 <d.xavero@hotmail.com>
2024-01-19 16:37:36 +0100

    ♻️ F08: Title > Refactor code and delete useless TitleProjects

--- src/components/reusable-ui/Title.jsx
+++ src/components/reusable-ui/Title.jsx
@@ -1,15 +1,28 @@
 /* eslint-disable react/prop-types */
-export default function Title({ className, label, description }) {
+export default function Title({
+  className,
+  label,
+  description,
+  variant = "normal",
+}) {
   return (
     <div
       className={`flex flex-col gap-2 ${
         className ? className : "text-4xl mb-20 text-center"
       }`}
     >
-      <h2 className="font-extrabold">
-        <span className="text-primary">/</span>
+      <h2 className="font-bold font-title">
+        <span
+          className={`text-primary ${
+            variant === "normal" || variant === "hidden" ? "hidden" : ""
+          }`}
+        >
+          /
+        </span>
         <span>{label}</span>
-        <span className="text-secondary">.</span>
+        <span className={variant === "hidden" ? "hidden" : "text-secondary"}>
+          .
+        </span>
       </h2>
       {description && <p className="text-xl">{description}</p>}
     </div>
