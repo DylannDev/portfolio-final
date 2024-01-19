a1e781d8f09740416732dd152c7578b9e495f276
dylann97 <d.xavero@hotmail.com>
2024-01-19 16:35:48 +0100

    🚧  F08: Button > replace a tag by Link provided by react router

--- src/components/reusable-ui/Button.jsx
+++ src/components/reusable-ui/Button.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { DarkModeContext } from "../../context/DarkModeContext";
+import { Link } from "react-router-dom";
 
 /* eslint-disable react/prop-types */
 export default function Button({
@@ -7,26 +8,24 @@ export default function Button({
   label,
   href,
   target,
-  download,
   onClick,
   variant = "normal",
 }) {
   const { darkButton, normalButton } = useContext(DarkModeContext);
 
   return (
-    <a
-      className={`flex items-center justify-center gap-2 rounded-full px-4 py-3 active:text-light hover:bg-transparent 
+    <Link
+      className={`flex items-center justify-center gap-2 rounded-full px-5 py-3 active:text-light hover:bg-transparent 
       border-[1px] transition-all duration-300 cursor-pointer shadow-md w-fit ${
         variant === "normal" ? normalButton : darkButton
       }`}
-      href={href}
-      download={download}
+      to={href}
       target={target}
       rel="noreferrer"
       onClick={onClick}
     >
       {Icon && <div className="text-xl">{Icon}</div>}
       <span className="">{label}</span>
-    </a>
+    </Link>
   );
 }
