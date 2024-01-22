27c291855a3a1ac1c9cea321bee2571f40268571
dylann97 <d.xavero@hotmail.com>
2024-01-22 19:02:26 +0100

    ✨ F09: Work > Add link of home page on logo

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { DarkModeContext } from "../../context/DarkModeContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Logo() {
+  const navigate = useNavigate();
   const { darkMode } = useContext(DarkModeContext);
   return (
     <img
       src={`/images/${darkMode ? "logo-dev-blanc.svg" : "logo-dev-black.svg"}`}
       alt="Logo Dylann Dev"
-      className="w-[250px]"
+      className="w-[250px] cursor-pointer"
+      onClick={() => navigate("/")}
     />
   );
 }
