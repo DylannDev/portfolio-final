2da634fb7d96be53756c9b989aa15714079b74b6
dylann97 <d.xavero@hotmail.com>
2024-01-25 21:42:47 +0100

    🐛  F10: About > fix error of import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import { useContext } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/pages/home/Home";
-import WorkPage from "./components/pages/work/WorkPage.jsx";
+import WorkPage from "./components/pages/work/WorkPage";
 import ErrorPage from "./components/pages/error/ErrorPage";
-import Layout from "./components/pages/layout/Layout.jsx";
-import { useContext } from "react";
-import { DarkModeContext } from "./context/DarkModeContext.jsx";
-import About from "./components/pages/about/about.jsx";
-import Project from "./components/pages/work/Projects/Project.jsx";
-import SkillsPage from "./components/pages/skills/skillsPage.jsx";
+import Layout from "./components/pages/layout/Layout";
+import { DarkModeContext } from "./context/DarkModeContext";
+import About from "./components/pages/about/About";
+import Project from "./components/pages/work/Projects/Project";
+import SkillsPage from "./components/pages/skills/SkillsPage";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
