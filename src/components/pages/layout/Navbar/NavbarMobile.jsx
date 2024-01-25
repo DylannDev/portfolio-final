ae38af53616c7b8591fc220dc7d981db46fb1429
dylann97 <d.xavero@hotmail.com>
2024-01-25 21:36:35 +0100

    🐛   F10: Navbar > fix bug of display

--- src/components/pages/layout/Navbar/NavbarMobile.jsx
+++ src/components/pages/layout/Navbar/NavbarMobile.jsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Logo from "../../../reusable-ui/Logo";
 import MenuBurger from "./MenuBurger";
 import NavbarLinksMobile from "./NavbarLinksMobile";
+import { DarkModeContext } from "../../../../context/DarkModeContext";
 
 export default function NavbarMobile() {
+  const { darkMode } = useContext(DarkModeContext);
   const [isMenuActive, setIsMenuActive] = useState(false);
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const handleMenuClicked = () => {
+    setIsMenuActive(!isMenuActive);
+
+    if (isMenuActive)
+      setTimeout(() => {
+        setMenuClicked(false);
+      }, 600);
+
+    setMenuClicked(true);
+  };
+
   return (
     <header className="lg:hidden flex justify-between items-center h-40 absolute top-0 left-0 right-0">
       <Logo />
-      <MenuBurger
+      <MenuBurger isMenuActive={isMenuActive} onClick={handleMenuClicked} />
+      <NavbarLinksMobile
         isMenuActive={isMenuActive}
-        onClick={() => setIsMenuActive(!isMenuActive)}
+        setIsMenuActive={setIsMenuActive}
+        menuClicked={menuClicked}
+        setMenuClicked={setMenuClicked}
+        darkMode={darkMode}
       />
-      <NavbarLinksMobile isMenuActive={isMenuActive} />
     </header>
   );
 }
