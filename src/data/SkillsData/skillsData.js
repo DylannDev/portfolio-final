db29c5b56128db4cec8c780c8d64b55b001125b1
dylann97 <d.xavero@hotmail.com>
2024-01-25 23:04:55 +0100

    🐛  F10: App > fix error of import (skillsData.js)

---
+++ src/data/SkillsData/skillsData.js
@@ -0,0 +1,57 @@
+import { FaGithub, FaFigma } from "react-icons/fa";
+import {
+  SiAdobephotoshop,
+  SiAdobeillustrator,
+  SiReact,
+  SiTailwindcss,
+  SiStyledcomponents,
+  SiFirebase,
+  SiNextdotjs,
+} from "react-icons/si";
+
+const skillsDataObject = {
+  "Front-End": [
+    {
+      icon: <SiReact />,
+      label: "React",
+    },
+    {
+      icon: <SiTailwindcss />,
+      label: "TailwindCSS",
+    },
+    {
+      icon: <SiStyledcomponents />,
+      label: "Styled-Components",
+    },
+  ],
+  "Back-End & Others": [
+    {
+      icon: <SiFirebase />,
+      label: "Firebase",
+    },
+    {
+      icon: <SiNextdotjs />,
+      label: "Next.js",
+    },
+    {
+      icon: <FaGithub />,
+      label: "Git & Github",
+    },
+  ],
+  Design: [
+    {
+      icon: <FaFigma />,
+      label: "Figma",
+    },
+    {
+      icon: <SiAdobephotoshop />,
+      label: "Photoshop",
+    },
+    {
+      icon: <SiAdobeillustrator />,
+      label: "Illustrator",
+    },
+  ],
+};
+
+export const skillsData = Object.entries(skillsDataObject);
