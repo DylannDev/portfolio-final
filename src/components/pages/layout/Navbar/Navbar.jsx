0e6990f3c59f1572b9e2ff62a18f23f94efc4248
dylann97 <d.xavero@hotmail.com>
2024-01-26 14:53:48 +0100

    ⚡️  F11: Navbar > Add a custom hook to display navbar components depending on the screen size

--- src/components/pages/layout/Navbar/Navbar.jsx
+++ src/components/pages/layout/Navbar/Navbar.jsx
@@ -1,11 +1,9 @@
+import useScreenSize from "../../../../hooks/useScreenSize";
 import NavbarDesktop from "./NavbarDesktop";
 import NavbarMobile from "./NavbarMobile";
 
 export default function Navbar() {
-  return (
-    <>
-      <NavbarDesktop />
-      <NavbarMobile />
-    </>
-  );
+  const screenSize = useScreenSize();
+
+  return screenSize.width > 1024 ? <NavbarDesktop /> : <NavbarMobile />;
 }
